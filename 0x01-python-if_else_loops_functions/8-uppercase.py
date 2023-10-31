d528238bc11a50dd8fb3d30e52678bb0d4efa830
mabakry <mabakry@gmail.com>
2023-10-31 20:30:52 +0000

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for x in str:
+        y = ord(x)
+        if y >= 97 and y <= 122:
+            y -= 32
+            print("{}".format(chr(y)), end='')
+        else:
+            print("{}".format(x), end='')
+    print()
