cbd837b8ddf2875fe5b6167ee0809469eeb70820
mabakry <mabakry@gmail.com>
2023-11-02 14:38:03 +0000

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    argv = sys.argv
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    elif len(argv) > 2:
+        print("{} argument:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
+            i += 1
