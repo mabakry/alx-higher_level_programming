47521238bbcc13b4a3082aa143bca8a669702bf2
mabakry <mabakry@gmail.com>
2023-11-04 15:13:47 +0000

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    x = len(matrix)
+    y = len(matrix[0])
+    for i in range(0, x):
+        for j in range(0, y):
+            print("{:d}".format(matrix[i][j]), end = "")
+            if j != y - 1:
+                print(" ", end = "")
+        print("$")
