57f6cd25737abea91b4cf0b82aa89cbbe6b5d849
mabakry <mabakry@gmail.com>
2023-11-04 14:43:27 +0000

    task 5 fix 2

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if 'c' in my_string:
-        index = my_string.find("c")
-        new_string = my_string[:index] + my_string[index + 1:]
-        if 'C' in new_string:
-            index = new_string.find("C")
-            new_string = new_string[:index] + new_string[index + 1:]
+    if my_string:
+        if 'c' in my_string:
+            index = my_string.find("c")
+            new_string = my_string[:index] + my_string[index + 1:]
+            if 'C' in new_string:
+                index = new_string.find("C")
+                new_string = new_string[:index] + new_string[index + 1:]
+                return (new_string)
+            else:
+                return (new_string)
+        elif 'C' in my_string:
+            index = my_string.find("C")
+            new_string = my_string[:index] + my_string[index + 1:]
             return (new_string)
         else:
-            return (new_string)
-    elif 'C' in my_string:
-        index = my_string.find("C")
-        new_string = my_string[:index] + my_string[index + 1:]
-        return (new_string)
-    else:
-        return (my_string)
+            return (my_string)
