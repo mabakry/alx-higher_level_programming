537c1896794b363fd0df04831c474c80ffb3b9cd
mabakry <mabakry@gmail.com>
2023-11-04 15:48:25 +0000

    task 6 fix 2

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     y = len(matrix[0])
     for i in range(0, x):
         for j in range(0, y):
-            print("{:d}".format(matrix[i][j]), end = "")
+            print("{:d}".format(matrix[i][j]), end="")
             if j != y - 1:
-                print(" ", end = "")
+                print(" ", end="")
         print()
