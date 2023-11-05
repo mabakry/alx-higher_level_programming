4bd4d7a50d3c2372b2f420d9a24e5e0164c88b62
mabakry <mabakry@gmail.com>
2023-11-05 11:13:02 +0000

    task 13 fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,13 +1,32 @@
 #include "lists.h"
 
 /**
+ * list_len - calculate list length.
+ * @head: list
+ * Return: list length
+ */
+
+int list_len(listint_t **head)
+{
+	int n = 0;
+
+	while (head && *head)
+	{
+		n++;
+		*head = (*head)->next;
+	}
+	return (n);
+}
+
+
+/**
  * is_palindrome -  checks if a singly linked list is a palindrome.
  * @head: list to be checked.
  * Return: 0 if not palindrome, 1 if palindrome.
  */
 int is_palindrome(listint_t **head)
 {
-	int new[100], i = 0;
+	int new[list_len(head)], i = 0;
 	listint_t *temp = *head, *cmp = *head;
 
 	if (*head == NULL || head == NULL)
