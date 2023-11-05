9e3688b829704989ad42af4ebef08e840b0e0661
mabakry <mabakry@gmail.com>
2023-11-05 12:05:16 +0000

    task 13 fix 4

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -22,6 +22,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-listint_t *add_node(listint_t **head, int n);
+listint_t *add_node(listint_t **head, const int n);
 
 #endif /* LISTS_H */
