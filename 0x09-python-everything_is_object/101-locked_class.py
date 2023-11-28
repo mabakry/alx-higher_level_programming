ea7665377b5c9d17ed7dd391dd097d02590c2a92
mabakry <mabakry@gmail.com>
2023-11-28 15:39:13 +0000

    task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" locked class module"""
+
+
 class LockedClass:
+    """prevents the user from dynamically creating new instance attributes
+     except if the new instance attribute is called first_name"""
+
     __slots__ = ["first_name"]
 
     def __init__(self):
