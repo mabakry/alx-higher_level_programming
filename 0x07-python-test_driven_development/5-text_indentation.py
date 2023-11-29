0514c97232823d29b3182b0da567762491f00e0d
mabakry <mabakry@gmail.com>
2023-11-29 19:42:00 +0000

    task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Print text Module"""
+
+
+def text_indentation(text):
+    """ prints a text with 2 new lines after
+    each of these characters: ., ? and :"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for i in range(0, len(text)):
+        if text[i] in ('.', '?', ':'):
+            print(text[i])
+            print()
+        elif text[i] == " " and text[i - 1] in ('.', '?', ':'):
+            pass
+        else:
+            if text[i - 1] not in ('.', '?', ':') or text[i] != " ":
+                print(text[i], end="")
+    print()
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
