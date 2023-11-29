12d50f5309e9501b0448efd47eabbc31a3f2b395
mabakry <mabakry@gmail.com>
2023-11-29 21:29:18 +0000

    task 2 fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,21 +2,31 @@
 def matrix_divided(matrix, div):
     """  divides all elements of a matrix."""
 
-    new_matrix = [[]]
+    m = "matrix must be a matrix (list of lists) of integers/floats"
+
     if type(div) not in (int, float):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    for i in matrix:
-        if i != 0 and len(i) != len(i - 1):
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("{}".format(m))
+
+    for row in matrix:
+        if not isinstance(row, list) or len(row) == 0:
+            raise TypeError("{}".format(m))
+
+        if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
 
-        for j in i:
+        for j in row:
             if type(j) not in (int, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("{}".format(m))
 
-            new_matrix.append("{:.2f}".format(j / div))
+    return [[round(j / div, 2) for j in row] for row in matrix]
 
-    return new_matrix
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
