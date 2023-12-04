c099f070ef92d975229be9c671f0e584ffc8f4fa
mabakry <mabakry@gmail.com>
2023-12-04 18:59:31 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""  class module"""
+
+
+class BaseGeometry:
+    """ raises an Exception"""
+
+    def area(self):
+        """ raises an Exception with the message area() is not implemented"""
+
+        raise Exception("area() is not implemented")
