44e91d3b290fb576668c251d69297de047e27d69
mabakry <mabakry@gmail.com>
2023-12-04 17:55:01 +0000

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/nib/python3
+""" returns the list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """  returns the list of available attributes and methods of an object"""
+
+    return dir(obj)
