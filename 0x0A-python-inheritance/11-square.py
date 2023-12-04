796bf52023eb392debcf7ac5cb09984368aff61a
mabakry <mabakry@gmail.com>
2023-12-04 21:35:01 +0000

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A Square class"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ A Square class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """ return a string"""
+        return ("[Square] {}/{}".format(self.__size, self.__size))
