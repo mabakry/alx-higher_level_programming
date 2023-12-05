c5a7e31b31e17ee0978be53483eef0afe6b50d7b
mabakry <mabakry@gmail.com>
2023-12-05 21:59:19 +0000

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" appends a string at the end of a text file"""
+
+
+def append_write(filename="", text=""):
+    """ appends a string at the end of a text file"""
+
+    with open(filename, 'a') as f:
+        f.write(text)
+        return len(text)
