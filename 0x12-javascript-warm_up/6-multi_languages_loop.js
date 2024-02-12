266d6bbcd24fa9b4319f7fe48f2d48c638b3b7b6
mabakry <mabakry@gmail.com>
2024-02-12 19:45:24 +0000

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 // prints 3 lines  by
 // using an array of string and a loop
 
-const l = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const l = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++) {
   console.log(l[i]);
