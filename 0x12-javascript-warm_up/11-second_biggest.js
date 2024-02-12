df5cc97cdb729774e847155f0bbc313f07ebceea
mabakry <mabakry@gmail.com>
2024-02-12 20:29:42 +0000

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
+const mySol = process.argv;
+if (mySol.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  console.log(mySol.sort((x, y) => y - x).slice(3)[0]);
 }
