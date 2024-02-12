0f8e04ca621a01e17910b4e7e2233cac135a9834
mabakry <mabakry@gmail.com>
2024-02-12 19:31:07 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-	console.log('No argument');
+/* prints a message depending
+of the number of arguments passed */
+const numArgv = process.argv.length;
+if (numArgv < 3) {
+  console.log('No argument');
 } else if (numArgv === 3) {
   console.log('Argument found');
 } else {
