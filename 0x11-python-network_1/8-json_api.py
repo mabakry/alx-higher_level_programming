ef4031b97856d300f25c1ee8827f4b231860bf37
mabakry <mabakry@gmail.com>
2024-03-20 13:03:22 +0000

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,19 +6,15 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv > 1):
-        letter = sys.argv[1]
-    else:
-        letter = ""
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-    value = {"q": letter}
-
-    r = requests.post("http://0.0.0.0:5000/search_user", data=value)
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
         response = r.json()
         if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id", response.get("name"))))
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
