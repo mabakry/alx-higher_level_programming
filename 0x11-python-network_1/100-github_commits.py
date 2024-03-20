26c13ef57ce4ace7473398d58663269322325234
mabakry <mabakry@gmail.com>
2024-03-20 13:37:08 +0000

    task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,10 +7,14 @@ import sys
 
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
-            sys.argv[2], sys.argv[1])
+        sys.argv[2], sys.argv[1])
 
     r = requests.get(url)
     commits = r.json()
-    for i in range(10):
-        print("{}: {}".format(commits[i].get(
-            "sha"), commits[i].get("author").get("name")))
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
