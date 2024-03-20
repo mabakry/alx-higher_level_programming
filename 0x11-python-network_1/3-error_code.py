861287893cc8cd076a9f1e09d900791b489e5a0d
mabakry <mabakry@gmail.com>
2024-03-20 12:00:08 +0000

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and 
+"""Python script that takes in a URL, sends a request to the URL and
 displays the body of the response (decoded in utf-8).
-
 You have to manage urllib.error.HTTPError exceptions and print:
 Error code: followed by the HTTP status code"""
 
