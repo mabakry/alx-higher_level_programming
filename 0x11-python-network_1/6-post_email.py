8dc92d91201548d591c22afb21c636247578db4f
mabakry <mabakry@gmail.com>
2024-03-20 12:36:23 +0000

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
-    r = requests.get(url, data=value)
+    r = requests.post(url, data=value)
     print(r.text)
