31885a6cf60bf794a6881ba4be1fd2bec074263a
mabakry <mabakry@gmail.com>
2024-03-20 13:35:13 +0000

    task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails”"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    for i in range(10):
+        print("{}: {}".format(commits[i].get(
+            "sha"), commits[i].get("author").get("name")))
